238c94065ab2093f0317f3f5976af1eaef2995c2
AxelWickman <axelwickm@gmail.com>
2023-03-09 16:37:50 +0100

    Fix bat file not executing all commands

--- UpdateGetReal.bat
+++ UpdateGetReal.bat
@@ -1,14 +1,12 @@
 @echo off
 
-echo Pulling latest version from GitHub
-git pull
-
-echo Installing dependencies for frontend serverx
-npm install
-
-echo Installing dependencies for backend server
-cd server
-npm install
+echo Pulling latest version from GitHub & ^
+git pull & ^
+echo Installing dependencies for frontend server & ^
+npm install & ^
+echo Installing dependencies for backend server & ^
+cd server & ^
+npm install & ^
 cd ..
 
 exit
