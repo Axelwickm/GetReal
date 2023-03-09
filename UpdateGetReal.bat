44d651bf8b6b7071da986bb35886d569ccb91fa1
AxelWickman <axelwickm@gmail.com>
2023-03-09 16:30:55 +0100

    Change update order

--- UpdateGetReal.bat
+++ UpdateGetReal.bat
@@ -1,9 +1,14 @@
 @echo off
 
+echo "Pulling latest version from GitHub""
+git pull
+
+echo "Installing dependencies for frontend server"
 npm install
+
+echo "Installing dependencies for backend server"
 cd server
 npm install
 cd ..
-git pull
 
 exit
