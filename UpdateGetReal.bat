05ba46ae68fb2cff99cadce5a282afd32925f0b7
AxelWickman <axelwickm@gmail.com>
2023-03-09 16:34:05 +0100

    Remove quotes from bash script

--- UpdateGetReal.bat
+++ UpdateGetReal.bat
@@ -1,12 +1,12 @@
 @echo off
 
-echo "Pulling latest version from GitHub""
+echo Pulling latest version from GitHub
 git pull
 
-echo "Installing dependencies for frontend server"
+echo Installing dependencies for frontend serverx
 npm install
 
-echo "Installing dependencies for backend server"
+echo Installing dependencies for backend server
 cd server
 npm install
 cd ..
